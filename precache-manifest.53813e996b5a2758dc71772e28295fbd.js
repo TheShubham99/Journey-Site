9f4266a4ca6cda9eb4d7b3bbd455c0f004da4de8
Pratham <ssahasrabhojane@gmail.com>
2020-06-02 20:54:38 +0530

    Updates

---
+++ precache-manifest.53813e996b5a2758dc71772e28295fbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b01d5ab909e189f59b1bdfef57e0555",
+    "url": "/Journey-Site/index.html"
+  },
+  {
+    "revision": "a83c643c428dd9d47d52",
+    "url": "/Journey-Site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b96f94202e9526e6fec8",
+    "url": "/Journey-Site/static/js/2.5ce6bacb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Journey-Site/static/js/2.5ce6bacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83c643c428dd9d47d52",
+    "url": "/Journey-Site/static/js/main.036324cd.chunk.js"
+  },
+  {
+    "revision": "0c7cfaaf14726ca71250",
+    "url": "/Journey-Site/static/js/runtime-main.91d8ef5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Journey-Site/static/media/logo.5d5d9eef.svg"
+  }
+]);
