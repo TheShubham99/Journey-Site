f3d8f4c8e5bece8ba33f053e77e1a0bc028b31dc
Pratham <ssahasrabhojane@gmail.com>
2020-06-05 20:48:00 +0530

    Decomposed landing page into Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/index.css';
 import { Navbar } from './components/Navbar';
 import { UserMessageDiv } from './components/UserMessageDiv';
 import { BottomNavbar } from './components/BottomNavbar';
