7d98e06628fa3a46936741296f0cdb9a5769f077
Pratham <ssahasrabhojane@gmail.com>
2020-06-05 20:39:53 +0530

    Move images to src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components/Navbar';
+import { UserMessageDiv } from './components/UserMessageDiv';
+import { BottomNavbar } from './components/BottomNavbar';
+import { Background } from './components/Background';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Pratham
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+
+      <Background>
+        <Navbar />
+        <UserMessageDiv />
+        <BottomNavbar />
+      </Background>
+
+    </React.Fragment>
+  )
 }
 
-export default App;
